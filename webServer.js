1235daf5f0ddd4af229b46af176dba50a894c1c4
GeoffGeis <geoffgeis@hotmail.com>
2015-06-14 15:31:20 -0700

    4 space indents

--- webServer.js
+++ webServer.js
@@ -1,8 +1,8 @@
 var http = require('http');
 
 function onListenEvent(req, res) {
-  res.writeHead(200, { 'Content-Type': 'text/html' });
-  res.end('<h1>hello world</h1>');
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.end('<h1>hello world</h1>');
 }
 
 var app = http.createServer(onListenEvent);
