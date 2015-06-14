9aef08aa1ad2d828a0302f07608d886e11d8e00d
GeoffGeis <geoffgeis@hotmail.com>
2015-06-14 15:52:43 -0700

    new files

---
+++ webServerFromIndex.js
@@ -0,0 +1,26 @@
+var http = require('http'),
+	fs = require('fs'),
+	path = require('path');
+
+var app = http.createServer(function(req, res) {
+	var index = path.join(__dirname, '/index.html');
+	if(req.url === '/' || req.url === '/index.html') {
+		fs.readFile(index, function(err, data) {
+			if(err) {
+				console.error(err);
+				res.writeHead(500, {'Content-Type': 'text/html'});
+				res.end('500 server error');
+			} else {
+				res.writeHead(200, {'Content-Type': 'text/html'});
+				res.end(data);
+			}
+		})
+	} else {
+		console.log('resource not found: ' + req.url);
+		res.writeHead(404, {'Content-Type': 'text/html'});
+		res.end(data);
+	}
+});
+
+app.listen(9001, 'localhost');
+console.log('server up at localhost:9001');
